f38af2a88c6f2b2c2cc48d73af78c3965bc331d3
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-12 22:45:29 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function firstWord(s) {
   // your code here
+	if(s === '' || s ===" "){
+		return '';
+	}
+	let ans ='';
+	for(let key of s){
+		if(key == ' '){
+			break ;
+		}else{
+			ans += key ;
+		}
+	}
+	return ans ;
 }
+
 
 // Do not change the code below
 
